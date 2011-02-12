e0684f02d34b8d9117c037f977eae9d2f0ae2dbb
tbonemalone <tony.org@gmail.com>
2011-02-12 10:15:23 -0800

    testing something

--- bonzai/urls.py
+++ bonzai/urls.py
@@ -1,4 +1,6 @@
 from django.conf.urls.defaults import *
+from django.contrib import admin # imports view for admin site
+admin.autodiscover() # added to automatically load the INSTALLED_APPS admin.py modules
 from seo_monitor import views
 
 # Uncomment the next two lines to enable the admin:
@@ -10,7 +12,7 @@ urlpatterns = patterns('',
 	(r'^signup/$', views.signup_page),
 	(r'^login/$', views.login_page),
 	(r'^logout/$', views.logout_page),
-	(r'^user/(\w+)/$', user_page) # parens capture string that matches. pass to view. 
+	(r'^user/(\w+)/$', views.user_page), # parens capture string that matches. pass to view. 
     # Example:
     # (r'^bonzai/', include('bonzai.foo.urls')),
 
