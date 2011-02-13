50a765ee0e98f0b527b753c405b8fd224f1cf6fb
tbonemalone <tony.org@gmail.com>
2011-02-13 07:38:25 -0800

    added models for Site and Pages. Import django User model

--- bonzai/seo_monitor/models.py
+++ bonzai/seo_monitor/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from django.contrib.auth.models import User 
 
 # Create your models here.
+
+class Site(models.Model):
+	"""modelf for a site"""
+	pass
+
+class Page(models.Model):
+	"""Model for a page"""
+	url = models.URLField()
+	keyword = models.CharField()
+	title_tag = models.CharField()
+	h1_tag = models.CharField()
+	last_date_audited = models.DateTimeField()
+	site = models.ForeignKey(Site)
+	
