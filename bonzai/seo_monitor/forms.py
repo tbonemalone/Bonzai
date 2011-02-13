d49e7103b06cb82d779a2e7ef44e920d8d87652f
tbonemalone <tony.org@gmail.com>
2011-02-13 10:55:24 -0800

    started work on the registration form.

--- bonzai/seo_monitor/forms.py
+++ bonzai/seo_monitor/forms.py
@@ -4,4 +4,7 @@ from django import forms
 
 class RegistrationForm(forms.Form):
 	"""Registration Form will verify password typed correctly."""
-	pass
+	username = forms.CharField(label=u'Username', max_lenght=30)
+	email = forms.EmailField(label=u'Email')
+	password1 = forms.Charfield(label=u'Password', widget=forms.PasswordInput())
+	password2 = forms.Charfield(label=u'Password Again'. widget=forms.PasswordInput())
