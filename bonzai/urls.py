264120b4db9b83d2a5c1fdf04a2d21e5a662d635
tbonemalone <tony.org@gmail.com>
2011-02-13 10:33:43 -0800

    add URLpattern for site_media directory

--- bonzai/urls.py
+++ bonzai/urls.py
@@ -1,7 +1,11 @@
+import os
 from django.conf.urls.defaults import *
 from django.contrib import admin # imports view for admin site
 admin.autodiscover() # added to automatically load the INSTALLED_APPS admin.py modules
 from seo_monitor import views
+
+site_media = os.path.join(os.path.dirname(__file__), 'site_media')
+
 
 # Uncomment the next two lines to enable the admin:
 # from django.contrib import admin
@@ -13,6 +17,8 @@ urlpatterns = patterns('',
 	(r'^login/$', 'django.contrib.auth.views.login'),
 	(r'^logout/$', views.logout_page),
 	(r'^user/(\w+)/$', views.user_page), # parens capture string that matches. pass to view. 
+	(r'^site_media/(?P<path>.*)$', 'django.views.static.serve',
+		{ 'document_root': site_media }),
     # Example:
     # (r'^bonzai/', include('bonzai.foo.urls')),
 
