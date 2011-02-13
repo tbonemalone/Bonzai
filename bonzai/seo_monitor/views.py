5de5c7f73cc0ad69e4ad5a39bf7a834ee2959d60
tbonemalone <tony.org@gmail.com>
2011-02-13 08:35:50 -0800

    pass username to home page welcome message upon login

--- bonzai/seo_monitor/views.py
+++ bonzai/seo_monitor/views.py
@@ -5,7 +5,8 @@ from django.shortcuts import render_to_response
 
 def home_page(request):
 	"""View for homepage"""
-	return render_to_response('home_page.html', locals())
+	other = request.META
+	return render_to_response('home_page.html', {'user': request.user, 'other': other})
 
 def login_page(request):
 	"""View for the login page"""
