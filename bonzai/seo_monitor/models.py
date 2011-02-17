5a1f8dcf28b203105079a4705afeadf507b02e1c
tbonemalone <tony.org@gmail.com>
2011-02-17 07:19:01 -0800

    added notes and questions

--- bonzai/seo_monitor/models.py
+++ bonzai/seo_monitor/models.py
@@ -2,12 +2,18 @@ from django.db import models
 from django.contrib.auth.models import User 
 
 # Create your models here.
+# work on models with ryan today. Need to get them set up correctly
 
 # rename this model django has a different site model that they give you.
 class Site(models.Model): 
 	"""model for a site"""
+	user = models.ForeignKey(User)
 	tld = models.URLField()
 	xml_sitemap_loc = models.URLField() #change name to xml_sitemap_uri
+	
+	def __unicode__(self):
+		"""docstring for __unicode__"""
+		return self.tld
 
 class Page(models.Model):
 	"""Model for a page"""
@@ -18,4 +24,7 @@ class Page(models.Model):
 	last_date_audited = models.DateTimeField()
 	site = models.ForeignKey(Site)
 	
+	def __unicode__(self):
+		"""docstring for __unicode__"""
+		return self.url
 # create new keyword model use many to many relationship for pages
